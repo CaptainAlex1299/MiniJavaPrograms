9962d88a883a8f0b88d29fe8f80d5b53c12fab77
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-01-04 13:38:08 +0200

    Chick - Order of initialization

--- Chick.java
+++ Chick.java
@@ -1,3 +1,14 @@
 public class Chick{
-	Private String name = "Fluffy";
+	private String name = "Fluffy";
+	{System.out.println("setting field");}
+	
+	public Chick(){
+		name = "Tiny";
+		System.out.println("setting constructor");
+	}
+	
+	public static void main(String[] args){
+		Chick chick = new Chick();
+		System.out.println(chick.name);
+	}
 }
