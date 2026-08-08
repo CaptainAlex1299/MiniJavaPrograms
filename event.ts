6a65e3ea34499d53def90b4d7905970283bf99ce
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-08-08 18:48:24 +0200

    removed random ts file

--- event.ts
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const form = document.getElementById('contnet') as HTMLFormElement | null;
-	
-	if(!form) {
-		console.warn("Form element not found');
-		return;
-	}
-});
