e2f8ffe6d31ab9e38e2be5110664a58e9b394656
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-08-09 16:32:21 +0200

    bitwise negation operator (~)

--- ComplementNegationOperators.java
+++ ComplementNegationOperators.java
@@ -5,5 +5,11 @@ public class ComplementNegationOperators {
         System.out.print(isAnimalASleep);
         isAnimalASleep = !isAnimalASleep;
         System.out.print(isAnimalASleep);
+
+        //negating operator ~
+        int number = 71;
+        int negated = ~number;
+        System.out.print(number);
+        System.out.print(negated);
     }
 }
