083b249cdd169a6fc7f9c49d53560f1cbff91a84
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-08-10 14:11:28 +0200

    negation operator (-), reverses the value of number

--- ComplementNegationOperators.java
+++ ComplementNegationOperators.java
@@ -2,14 +2,21 @@
 public class ComplementNegationOperators {
     public static void main(String[] args) {
         boolean isAnimalASleep = false;
-        System.out.print(isAnimalASleep);
+        System.out.println(isAnimalASleep);
         isAnimalASleep = !isAnimalASleep;
-        System.out.print(isAnimalASleep);
+        System.out.println(isAnimalASleep);
 
         //negating operator ~
         int number = 71;
         int negated = ~number;
-        System.out.print(number);
-        System.out.print(negated);
+        System.out.println(number);
+        System.out.println(negated);
+
+        double zooTemperature = 1.21;
+        System.out.println(zooTemperature); // 1.21
+        zooTemperature = -zooTemperature;
+        System.out.println(zooTemperature); // -1.21
+        zooTemperature = -(-zooTemperature);
+        System.out.println(zooTemperature); // -1.21
     }
 }
