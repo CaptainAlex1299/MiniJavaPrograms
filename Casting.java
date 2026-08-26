10d638187b5a9bbe89a1a6ce670171dcb5416652
CaptainAlex1299 <alexanderdietstein@gmail.com>
2026-08-26 16:46:33 +0200

    casted long to an int

--- Casting.java
+++ Casting.java
@@ -1,7 +1,14 @@
 	public class Casting {
-	public static void main(String[] args){
-		double number = 6.7;
-		int newNumber = (int) number;
-		System.out.println(newNumber);
-	}
+		public static void main(String[] args){
+			double number = 6.7;
+			int newNumber = (int) number;
+			System.out.println(newNumber);
+			
+			long aboutToBeSmall = 19230134521234115L;
+			long bigNumber = 19230134521234113L;
+			long smallerNumber = aboutToBeSmall - bigNumber;
+			int castedSmallerNumber = (int) smallerNumber;
+			System.out.println(bigNumber);
+			System.out.println(castedSmallerNumber);
+		}
 	}
